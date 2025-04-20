539ef9002b04fc801b60136192a616404ab17e58
Soham Govande <vgsoham@gmail.com>
2025-04-20 15:47:33 -0700

    fix import error

--- src/chipmunk/ops/__init__.py
+++ src/chipmunk/ops/__init__.py
@@ -4,4 +4,4 @@ from .attn import csp_attn, dense_attn, dense_colsum_attn
 from .patch import patchify, unpatchify, patchify_rope
 from .bitpack import bitpack, bitunpack
 
-__all__ = ['mlp', 'copy_indices', 'topk_indices', 'mask_to_indices', 'scatter_add', 'csp_attn', 'dense_attn', 'dense_colsum_attn', 'patch', 'unpatchify', 'patchify_rope', 'bitpack', 'bitunpack']
+__all__ = ['mlp', 'copy_indices', 'topk_indices', 'mask_to_indices', 'scatter_add', 'csp_attn', 'dense_attn', 'dense_colsum_attn', 'patchify', 'unpatchify', 'patchify_rope', 'bitpack', 'bitunpack']
