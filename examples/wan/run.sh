f33246b061747186c4956f1b190132a334d07913
Soham Govande <vgsoham@gmail.com>
2025-04-24 18:02:59 -0700

    OOMs fixed for wan video

--- examples/wan/run.sh
+++ examples/wan/run.sh
@@ -1,3 +1,6 @@
+# avoid OOMs
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+
 # python generate.py  --task t2v-1.3B --size "832*480" --ckpt_dir ./Wan2.1-T2V-1.3B --offload_model True --t5_cpu --sample_shift 8 --sample_guide_scale 6 --prompt "Two anthropomorphic cats in comfy boxing gear and bright gloves fight intensely on a spotlighted stage."
 
 python generate.py  --task t2v-14B --size 1280*720 --ckpt_dir ./Wan2.1-T2V-14B --prompt "Two anthropomorphic cats in comfy boxing gear and bright gloves fight intensely on a spotlighted stage." --offload_model True
