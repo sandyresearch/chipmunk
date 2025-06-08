1964d41563141813bdde1e14114ecce4ab5e8c54
Soham Govande <vgsoham@gmail.com>
2025-06-07 21:56:51 -0500

    hopper check removed for flux

--- examples/flux/src/flux/cli.py
+++ examples/flux/src/flux/cli.py
@@ -135,10 +135,6 @@ def main(
     import chipmunk.util.config
     chipmunk.util.config.load_from_file(kwargs.get("chipmunk_config", "chipmunk-config.yml"))
     
-    device_props = torch.cuda.get_device_properties(torch.device(device))
-    sm_major = device_props.major
-    if sm_major < 9:
-        raise ValueError("Running Chipmunk requires an H100 GPU (SM90 or higher). Your GPU has compute capability SM{sm_major}X.")
     if name == 'flux-schnell' or num_steps < 10:
         print("CHIPMUNK: Warning - using Flux-schnell or a low number of steps may result in suboptimal performance. Proceed with caution unless you know what you're doing.")
     prompt = prompt.split("|")
