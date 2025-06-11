99684564d1bf51d04a80a6a5c2b4d476666afc71
Soham Govande <vgsoham@gmail.com>
2025-06-11 17:04:10 -0500

    create separate dirs per model

--- run_all.sh
+++ run_all.sh
@@ -5,8 +5,12 @@ bash ./run.sh
 cd ../wan
 bash ./run.sh
 cd ../../
-mkdir -p results_all
 
-mv examples/flux/output/* results_all/
-mv examples/hunyuan/output/* results_all/
-mv examples/wan/*.mp4 results_all/
+mkdir -p results_all
+mkdir -p results_all/flux
+mkdir -p results_all/hunyuan
+mkdir -p results_all/wan
+
+mv examples/flux/output/* results_all/flux/
+mv examples/hunyuan/output/* results_all/hunyuan/
+mv examples/wan/*.mp4 results_all/wan/
